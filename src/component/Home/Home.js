ca234ce0eb3046cebc78b9563b782fef6c312ca2
Ayushkumar025 <ayushkumar0117@gmail.com>
2024-09-16 09:45:40 +0530

    commiting

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -8,21 +8,47 @@ function Home() {
       <section className='main'>
         <div className='first'>
           <div className='first-img'>
-            <img src='./images/goat.jpg' width={'auto'} height={'auto'} alt='goat' className='image-1'></img>
-            <div className='text-overlay'>The Top Toy List is here</div>
+            <a href='#'><img src='./images/goat.jpg' alt='goat' className='image-1'></img></a>
+            <div className='text-overlay'>
+              <h6>The Top Toy List is <br></br> here</h6>
+              <a href='#'>Shop now</a>
+            </div>
           </div>
           <div className='second-img'>
-            <img src='./images/small-kid.jpg' width={'auto'} height={'auto'} alt='small-kid'></img>
+            <a href='#'><img src='./images/small-kid.jpg' alt='small-kid'></img></a>
           </div>
           <div className='third-img'>
-            <img src='./images/dino.jpg' width={'auto'} height={'auto'} alt='dinosaur'></img>
+            <a href='#'><img src='./images/dino.jpg' alt='dinosaur'></img></a>
           </div>
         </div>
         <div className='second'>
-
+          <div className='II-img-1'>
+            <a href='#'><img src='./images/iphone-16.jpg' alt='iphone-16'></img></a>
+          </div>
+          <div className='II-img-2'>
+            <div className='inner-div'>
+              <div className='inner-image-1'>
+                <a href='#'><img src='./images/bottles.jpg'></img></a>
+              </div>
+              <div className='inner-image-2'>
+                <a href='#'><img src='./images/purse.jpg'></img></a>
+              </div>
+            </div>
+          </div>
+          <div className='II-img-3'>
+            <a href='#'><img src='./images/walmart+.webp'></img></a>
+          </div>
         </div>
         <div className='third'>
-
+          <div className='III-img-1'>
+            <a href='#'><img src='./images/lady.jpg'></img></a>
+          </div>
+          <div className='III-img-2'>
+            <a href='#'><img src='./images/products.jpg'></img></a>
+          </div>
+          <div className='III-img-3'>
+            <a href='#'><img src='./images/lebron.jpg'></img></a>
+          </div>
         </div>
       </section>
     </main>
