62b7e6e737a70659e5d56fa71c8fb5382fc71df7
Ayushkumar025 <ayushkumar0117@gmail.com>
2024-09-22 21:13:12 +0530

    fifth

--- src/component/singleproducts/SingleProduct.js
+++ src/component/singleproducts/SingleProduct.js
@@ -1,14 +1,33 @@
 import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import './SingleProduct.css'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 function SingleProduct() {
-    const {state}=useLocation();  
+    const {state}=useLocation(); 
+    const product = {
+      desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 1,
+        slidesToSlide: 3 // optional, default to 1.
+      },
+      tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 1,
+        slidesToSlide: 2 // optional, default to 1.
+      },
+      mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1,
+        slidesToSlide: 1 // optional, default to 1.
+      }
+    };
   return (
     <>
         <main className='product-container'>
           <div className='container'>
-            <div className='sub-container'>
+            <div className='box-1'>
               <div className='sub'>
                 <img src='./images/sub-1.jpeg'></img>
               </div>
@@ -23,9 +42,55 @@ function SingleProduct() {
               </div>
             </div>
             <div className='box-2'>
-              <img src='./images/h-1.webp'></img>
+              <Carousel
+                responsive={product}
+                infinite={true}
+              > 
+                <div className='item'>
+                  <div className='image'>
+                    <img src='./images/h-1.webp'></img>
+                    </div>
+                </div>
+                <div className='item'>
+                  <div className='image'>
+                    <img src='./images/h-2.webp'></img>
+                    </div>
+                </div>
+                <div className='item'>
+                  <div className='image'>
+                    <img src='./images/h-3.webp'></img>
+                    </div>
+                </div>
+                <div className='item'>
+                  <div className='image'>
+                    <img src='./images/h-4.webp'></img>
+                    </div>
+                </div>
+              </Carousel>
             </div>
-            <div className='box-3'></div>
+            <div className='box-3'>
+              <div className='div-1'>
+                <span className='best-seller'>
+                  Best seller
+                </span>
+              </div>
+              <div className='div-2'>
+                <a href='#'>Way To Celebrate</a>
+              </div>
+              <div className='div-3'>
+                <h1>Halloween 10-Count Orange Pumpkin Lantern LED Lights, 6.5', by Way To Celebrate</h1>
+              </div>
+              <div className='div-4'>
+                <img src='./images/refresh.svg'></img>
+                <span>Free 90-day returns</span>
+              </div>
+              <div className='div-5'>
+                <h1>$10.98</h1>
+              </div>
+              <div className='div-6'>
+                <span>Add to cart</span>
+              </div>
+            </div>
           </div>
         </main>
     </>
